e888edac21fa3ed967f74c0f79c9c10f0104d52b
AbenezerM <abenezermulugeta41@gmail.com>
2024-03-20 16:47:25 -0400

    Import RouterModule and inject routes to app.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {HttpClientModule} from '@angular/common/http';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { ProductService } from './services/product.service';
+
+const routes: Routes = [
+  { path: 'category/:id', component: ProductListComponent },
+  { path: 'category', component: ProductListComponent },
+  { path: 'products', component: ProductListComponent },
+  { path: '', redirectTo: '/products', pathMatch: 'full' },
+  { path: '**', redirectTo: '/products', pathMatch: 'full' }
+]
 
 @NgModule({
   declarations: [
@@ -13,7 +22,8 @@ import { ProductService } from './services/product.service';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [
     ProductService
